3f95f9d16ca80da9ff0b7288f869ed5e1bf560a4
SithLor <84148006+SithLor@users.noreply.github.com>
2023-12-19 18:22:19 +0000

    e

---
+++ download.sh
@@ -0,0 +1,5 @@
+#https://www.arduino.cc/reference/en/libraries/adafruit-liquidcrystal/
+mkdir lib
+wget https://downloads.arduino.cc/libraries/github.com/adafruit/Adafruit_LiquidCrystal-2.0.4.zip
+unzip Adafruit_LiquidCrystal-2.0.4.zip -d ./lib
+rm Adafruit_LiquidCrystal-2.0.4.zip
